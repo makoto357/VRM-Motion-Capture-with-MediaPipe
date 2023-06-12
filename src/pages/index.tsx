44575948d6547630d2e383da6971c5c3db2677eb
Claire Chu(朱晏禛) <claire_chu@htc.com>
2023-06-12 17:57:44 +0800

    dynamic import canvas and create holistic instance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,93 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image';
+import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
-import * as Kalidokit from 'kalidokit';
-import * as THREE from 'three';
-import {useEffect} from 'react';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import {VRMLoaderPlugin} from '@pixiv/three-vrm';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
-
-//Import Helper Functions from Kalidokit
-// const clamp = Kalidokit.Utils.clamp;
-// const lerp = Kalidokit.Vector.lerp;
+import dynamic from 'next/dynamic';
+const KalidoCanvas = dynamic(() => import('./KalidoCanvas/index.page'), {ssr: false});
 
 export default function Home() {
-  // set up three.js once the canvas is loaded
-  useEffect(() => {
-    /* THREEJS WORLD SETUP */
-    let currentVrm;
-    // scene
-    const scene = new THREE.Scene();
-    // camera
-    const orbitCamera = new THREE.PerspectiveCamera(
-      35,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000,
-    );
-    orbitCamera.position.set(0.0, 0.0, 5);
-    // controls
-    const canvas = document.getElementById('myAvatar');
-    const orbitControls = new OrbitControls(orbitCamera, canvas as any);
-    orbitControls.screenSpacePanning = true;
-    orbitControls.target.set(0.0, 1.4, 0.0);
-    // call update method everytime we change the position of the camera
-    orbitControls.update();
-
-    // renderer
-    // An alpha value of 0.0 would result in the object having complete transparency.
-    // When set to true, the value is 0. Otherwise it's 1. Default is false.
-    // {alpha: true}?
-    const renderer = new THREE.WebGLRenderer({canvas: canvas as any});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    // light
-    const light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(1.0, 1.0, 1.0).normalize();
-    scene.add(light);
-    renderer.setClearColor(0xffea00);
-
-    // Main Render Loop
-    const clock = new THREE.Clock();
-    function animate() {
-      requestAnimationFrame(animate);
-
-      // if (currentVrm) {
-      //   // Update model to render physics
-      //   currentVrm.update(clock.getDelta());
-      // }
-      renderer.render(scene, orbitCamera);
-    }
-    animate();
-    /* VRM CHARACTER SETUP */
-    // Import Character VRM
-    const loader = new GLTFLoader();
-    loader.crossOrigin = 'anonymous';
-    // install plugin
-    loader.register(parser => {
-      return new VRMLoaderPlugin(parser);
-    });
-    // Import model from URL, add your own model here
-    loader.load(
-      'https://cdn.glitch.com/29e07830-2317-4b15-a044-135e73c7f840%2FAshtra.vrm?v=1630342336981',
-
-      gltf => {
-        // THREE.VRMUtils.removeUnnecessaryJoints(gltf.scene);
-        const vrm = gltf.userData.vrm;
-        // THREE.VRM.from(gltf).then(vrm => {
-        scene.add(vrm.scene);
-        console.log(vrm);
-        currentVrm = vrm;
-        currentVrm.scene.rotation.y = Math.PI; // Rotate model 180deg to face camera
-        // });
-      },
-
-      progress => console.log('Loading model...', 100.0 * (progress.loaded / progress.total), '%'),
-      // undefined,
-      error => console.error(error),
-    );
-  }, []);
   return (
     <>
       <Head>
@@ -97,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <canvas id="myAvatar" className={`${styles.myAvatar}`} />
+        <KalidoCanvas />
       </main>
     </>
   );
