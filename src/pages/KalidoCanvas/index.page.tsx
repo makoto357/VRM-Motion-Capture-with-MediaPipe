b180482ce6e3db1b3efec32b05ad1af90924add3
Claire Chu(朱晏禛) <claire_chu@htc.com>
2023-06-13 16:23:40 +0800

    animate vrm

--- src/pages/KalidoCanvas/index.page.tsx
+++ src/pages/KalidoCanvas/index.page.tsx
@@ -1,3 +1,4 @@
+import styles from '@/styles/Home.module.css';
 import {useEffect} from 'react';
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
@@ -5,7 +6,12 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {VRMExpressionPresetName, VRMLoaderPlugin, VRMUtils} from '@pixiv/three-vrm';
 import type * as V1VRMSchema from '@pixiv/types-vrmc-vrm-1.0';
 import * as Kalidokit from 'kalidokit';
-import {FACEMESH_TESSELATION, Holistic, POSE_CONNECTIONS} from '@mediapipe/holistic';
+import {
+  FACEMESH_TESSELATION,
+  HAND_CONNECTIONS,
+  Holistic,
+  POSE_CONNECTIONS,
+} from '@mediapipe/holistic';
 import {Camera} from '@mediapipe/camera_utils';
 import {drawConnectors, drawLandmarks, NormalizedLandmarkList} from '@mediapipe/drawing_utils';
 //Import Helper Functions from Kalidokit
@@ -88,15 +94,251 @@ export default function KalidoCanvas() {
       // undefined,
       error => console.error(error),
     );
+    // Animate Rotation Helper function
+    const rigRotation = (
+      name: string,
+      rotation = {x: 0, y: 0, z: 0},
+      dampener = 1,
+      lerpAmount = 0.3,
+    ) => {
+      if (!currentVrm) {
+        return;
+      }
+      const Part = currentVrm.humanoid.getNormalizedBoneNode(name);
+      console.log(Part);
+      if (!Part) {
+        return;
+      }
+
+      let euler = new THREE.Euler(
+        rotation.x * dampener,
+        rotation.y * dampener,
+        rotation.z * dampener,
+      );
+      let quaternion = new THREE.Quaternion().setFromEuler(euler);
+      Part.quaternion.slerp(quaternion, lerpAmount); // interpolate
+    };
+
+    // Animate Position Helper Function
+    const rigPosition = (
+      name: string,
+      position = {x: 0, y: 0, z: 0},
+      dampener = 1,
+      lerpAmount = 0.3,
+    ) => {
+      if (!currentVrm) {
+        return;
+      }
+      const Part = currentVrm.humanoid.getNormalizedBoneNode(name);
+      if (!Part) {
+        return;
+      }
+      let vector = new THREE.Vector3(
+        position.x * dampener,
+        position.y * dampener,
+        position.z * dampener,
+      );
+      Part.position.lerp(vector, lerpAmount); // interpolate
+    };
+
+    let oldLookTarget = new THREE.Euler();
+    const rigFace = (riggedFace: any) => {
+      if (!currentVrm) {
+        return;
+      }
+      rigRotation('neck', riggedFace.head, 0.7);
+      console.log(riggedFace.head);
+
+      // Blendshapes and Preset Name Schema
+      const Blendshape = currentVrm.expressionManager;
+      const PresetName = VRMExpressionPresetName;
+      // Simple example without winking. Interpolate based on old blendshape, then stabilize blink with `Kalidokit` helper function.
+      // for VRM, 1 is closed, 0 is open.
+      riggedFace.eye.l = lerp(
+        clamp(1 - riggedFace.eye.l, 0, 1),
+        Blendshape.getValue(PresetName.Blink),
+        0.5,
+      );
+      riggedFace.eye.r = lerp(
+        clamp(1 - riggedFace.eye.r, 0, 1),
+        Blendshape.getValue(PresetName.Blink),
+        0.5,
+      );
+      riggedFace.eye = Kalidokit.Face.stabilizeBlink(riggedFace.eye, riggedFace.head.y);
+      Blendshape.setValue(PresetName.Blink, riggedFace.eye.l);
+
+      // Interpolate and set mouth blendshapes
+      Blendshape.setValue(
+        PresetName.Ih,
+        lerp(riggedFace.mouth.shape.I, Blendshape.getValue(PresetName.Ih), 0.5),
+      );
+      Blendshape.setValue(
+        PresetName.Aa,
+        lerp(riggedFace.mouth.shape.A, Blendshape.getValue(PresetName.Aa), 0.5),
+      );
+      Blendshape.setValue(
+        PresetName.Ee,
+        lerp(riggedFace.mouth.shape.E, Blendshape.getValue(PresetName.Ee), 0.5),
+      );
+      Blendshape.setValue(
+        PresetName.Oh,
+        lerp(riggedFace.mouth.shape.O, Blendshape.getValue(PresetName.Oh), 0.5),
+      );
+      Blendshape.setValue(
+        PresetName.Ou,
+        lerp(riggedFace.mouth.shape.U, Blendshape.getValue(PresetName.Ou), 0.5),
+      );
+
+      //PUPILS
+      //interpolate pupil and keep a copy of the value
+      let lookTarget = new THREE.Euler(
+        lerp(oldLookTarget.x, riggedFace.pupil.y, 0.4),
+        lerp(oldLookTarget.y, riggedFace.pupil.x, 0.4),
+        0,
+        'XYZ',
+      );
+      oldLookTarget.copy(lookTarget);
+      currentVrm.lookAt?.applier.lookAt(lookTarget);
+    };
+
+    /* VRM Character Animator */
+    const animateVRM = (
+      vrm: any,
+      results: {
+        faceLandmarks: any;
+        ea: any;
+        poseLandmarks: any;
+        rightHandLandmarks: any;
+        leftHandLandmarks: any;
+      },
+    ) => {
+      if (!vrm) {
+        return;
+      }
+
+      //
+      // Take the results from `Holistic` and animate character based on its Face, Pose, and Hand Keypoints.
+      let riggedPose, riggedLeftHand, riggedRightHand, riggedFace;
+
+      const faceLandmarks = results.faceLandmarks;
+      // Pose 3D Landmarks are with respect to Hip distance in meters
+      const pose3DLandmarks = results.ea;
+      // Pose 2D landmarks are with respect to videoWidth and videoHeight
+      const pose2DLandmarks = results.poseLandmarks;
+      // Be careful, hand landmarks may be reversed
+      const leftHandLandmarks = results.leftHandLandmarks;
+      const rightHandLandmarks = results.rightHandLandmarks;
+
+      // Animate Face
+
+      if (faceLandmarks) {
+        riggedFace = Kalidokit.Face.solve(faceLandmarks, {
+          runtime: 'mediapipe',
+          video: videoElement,
+        });
+        rigFace(riggedFace);
+      }
+
+      // Animate Pose
+      if (pose2DLandmarks && pose3DLandmarks) {
+        riggedPose = Kalidokit.Pose.solve(pose3DLandmarks, pose2DLandmarks, {
+          runtime: 'mediapipe',
+          video: videoElement,
+        });
+        if (riggedPose) {
+          rigRotation('hips', riggedPose.Hips.rotation, 0.7);
+          rigPosition(
+            'hips',
+            {
+              x: -riggedPose.Hips.position.x, // Reverse direction
+              y: riggedPose.Hips.position.y + 1, // Add a bit of height
+              z: -riggedPose.Hips.position.z, // Reverse direction
+            },
+            1,
+            0.07,
+          );
+
+          rigRotation('chest', riggedPose.Spine, 0.25, 0.3);
+          rigRotation('spine', riggedPose.Spine, 0.45, 0.3);
+
+          rigRotation('rightUpperArm', riggedPose.RightUpperArm, 1, 0.3);
+          rigRotation('rightLowerArm', riggedPose.RightLowerArm, 1, 0.3);
+          rigRotation('leftUpperArm', riggedPose.LeftUpperArm, 1, 0.3);
+          rigRotation('leftLowerArm', riggedPose.LeftLowerArm, 1, 0.3);
+
+          rigRotation('leftUpperLeg', riggedPose.LeftUpperLeg, 1, 0.3);
+          rigRotation('leftLowerLeg', riggedPose.LeftLowerLeg, 1, 0.3);
+          rigRotation('rightUpperLeg', riggedPose.RightUpperLeg, 1, 0.3);
+          rigRotation('rightLowerLeg', riggedPose.RightLowerLeg, 1, 0.3);
+        }
+      }
+
+      // Animate Hands
+      if (leftHandLandmarks) {
+        riggedLeftHand = Kalidokit.Hand.solve(leftHandLandmarks, 'Left');
+        console.log('lefthand');
+        if (riggedLeftHand && riggedPose) {
+          rigRotation('leftHand', {
+            // Combine pose rotation Z and hand rotation X Y
+            z: riggedPose.LeftHand.z,
+            y: riggedLeftHand.LeftWrist.y,
+            x: riggedLeftHand.LeftWrist.x,
+          });
+          rigRotation('leftRingProximal', riggedLeftHand.LeftRingProximal);
+          rigRotation('leftRingIntermediate', riggedLeftHand.LeftRingIntermediate);
+          rigRotation('leftRingDistal', riggedLeftHand.LeftRingDistal);
+          rigRotation('leftIndexProximal', riggedLeftHand.LeftIndexProximal);
+          rigRotation('leftIndexIntermediate', riggedLeftHand.LeftIndexIntermediate);
+          rigRotation('leftIndexDistal', riggedLeftHand.LeftIndexDistal);
+          rigRotation('leftMiddleProximal', riggedLeftHand.LeftMiddleProximal);
+          rigRotation('leftMiddleIntermediate', riggedLeftHand.LeftMiddleIntermediate);
+          rigRotation('leftMiddleDistal', riggedLeftHand.LeftMiddleDistal);
+          rigRotation('leftThumbProximal', riggedLeftHand.LeftThumbProximal);
+          rigRotation('leftThumbIntermediate', riggedLeftHand.LeftThumbIntermediate);
+          rigRotation('leftThumbDistal', riggedLeftHand.LeftThumbDistal);
+          rigRotation('leftLittleProximal', riggedLeftHand.LeftLittleProximal);
+          rigRotation('leftLittleIntermediate', riggedLeftHand.LeftLittleIntermediate);
+          rigRotation('leftLittleDistal', riggedLeftHand.LeftLittleDistal);
+        }
+      }
+      if (rightHandLandmarks) {
+        riggedRightHand = Kalidokit.Hand.solve(rightHandLandmarks, 'Right');
+        rigRotation('rightHand');
+        if (riggedRightHand && riggedPose) {
+          console.log(riggedRightHand);
+          rigRotation('rightHand', {
+            // Combine Z axis from pose hand and X/Y axis from hand wrist rotation
+            z: riggedPose.RightHand.z,
+            y: riggedRightHand.RightWrist.y,
+            x: riggedRightHand.RightWrist.x,
+          });
+          rigRotation('rightRingProximal', riggedRightHand.RightRingProximal);
+          rigRotation('rightRingIntermediate', riggedRightHand.RightRingIntermediate);
+          rigRotation('rightRingDistal', riggedRightHand.RightRingDistal);
+          rigRotation('rightIndexProximal', riggedRightHand.RightIndexProximal);
+          rigRotation('rightIndexIntermediate', riggedRightHand.RightIndexIntermediate);
+          rigRotation('rightIndexDistal', riggedRightHand.RightIndexDistal);
+          rigRotation('rightMiddleProximal', riggedRightHand.RightMiddleProximal);
+          rigRotation('rightMiddleIntermediate', riggedRightHand.RightMiddleIntermediate);
+          rigRotation('rightMiddleDistal', riggedRightHand.RightMiddleDistal);
+          rigRotation('rightThumbProximal', riggedRightHand.RightThumbProximal);
+          rigRotation('rightThumbIntermediate', riggedRightHand.RightThumbIntermediate);
+          rigRotation('rightThumbDistal', riggedRightHand.RightThumbDistal);
+          rigRotation('rightLittleProximal', riggedRightHand.RightLittleProximal);
+          rigRotation('rightLittleIntermediate', riggedRightHand.RightLittleIntermediate);
+          rigRotation('rightLittleDistal', riggedRightHand.RightLittleDistal);
+        }
+      }
+    };
     /* SETUP MEDIAPIPE HOLISTIC INSTANCE */
-    let videoElement = document.querySelector('.input_video') as HTMLVideoElement,
-      guideCanvas = document.querySelector('canvas.guides') as HTMLCanvasElement;
+    let videoElement = document.querySelector('.input_video') as HTMLVideoElement;
+    let guideCanvas = document.querySelector('canvas.guides') as HTMLCanvasElement;
 
     const onResults = (results: any) => {
-      // Draw landmark guides
+      console.log(results);
       // drawResults(results);
       // Animate model
-      // animateVRM(currentVrm, results);
+      animateVRM(currentVrm, results);
     };
     const holistic = new Holistic({
       locateFile: file => {
@@ -114,6 +356,57 @@ export default function KalidoCanvas() {
     // Pass holistic a callback function
     holistic.onResults(onResults);
 
+    const drawResults = (results: {
+      poseLandmarks: NormalizedLandmarkList | undefined;
+      faceLandmarks: NormalizedLandmarkList | undefined;
+      leftHandLandmarks: NormalizedLandmarkList | undefined;
+      rightHandLandmarks: NormalizedLandmarkList | undefined;
+    }) => {
+      // guideCanvas.width = videoElement.videoWidth;
+      // guideCanvas.height = videoElement.videoHeight;
+      let canvasCtx = guideCanvas.getContext('2d') as CanvasRenderingContext2D;
+      if (canvasCtx) {
+        canvasCtx.save();
+        canvasCtx.clearRect(0, 0, guideCanvas.width, guideCanvas.height);
+        // Use `Mediapipe` drawing functions
+        drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+          color: '#00cff7',
+          lineWidth: 4,
+        });
+        drawLandmarks(canvasCtx, results.poseLandmarks, {
+          color: '#ff0364',
+          lineWidth: 2,
+        });
+        drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION, {
+          color: '#C0C0C070',
+          lineWidth: 1,
+        });
+        if (results.faceLandmarks && results.faceLandmarks.length === 478) {
+          //draw pupils
+          drawLandmarks(canvasCtx, [results.faceLandmarks[468], results.faceLandmarks[468 + 5]], {
+            color: '#ffe603',
+            lineWidth: 2,
+          });
+        }
+        drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, {
+          color: '#eb1064',
+          lineWidth: 5,
+        });
+        drawLandmarks(canvasCtx, results.leftHandLandmarks, {
+          color: '#00cff7',
+          lineWidth: 2,
+        });
+        drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS, {
+          color: '#22c3e3',
+          lineWidth: 5,
+        });
+        drawLandmarks(canvasCtx, results.rightHandLandmarks, {
+          color: '#ff0364',
+          lineWidth: 2,
+        });
+      }
+    };
+
     // Use `Mediapipe` utils to get camera - lower resolution = higher fps
     const camera = new Camera(videoElement as HTMLVideoElement, {
       onFrame: async () => {
@@ -125,7 +418,7 @@ export default function KalidoCanvas() {
     camera.start();
   }, []);
   return (
-    <div>
+    <div className={`${styles.scene}`}>
       <video
         className="input_video"
         width="1280px"
